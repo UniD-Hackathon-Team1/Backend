f12a2cfd5171cc894a7b33717f529f89db5ccff6
impala <100896063+iimpala@users.noreply.github.com>
2022-11-05 17:04:24 +0900

    Add files via upload

---
+++ src/main/java/UNID_1/FloatingLetter/FloatingLetterApplication.java
@@ -0,0 +1,13 @@
+package UNID_1.FloatingLetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloatingLetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloatingLetterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/UNID_1/FloatingLetter/FloatingLetterApplicationTests.java
@@ -0,0 +1,13 @@
+package UNID_1.FloatingLetter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloatingLetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
